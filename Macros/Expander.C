d1b40bc24ad6911ee73bdcd7404cfa07dcfaffea
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2023-06-21 19:30:53 +0200

    Add expanders

---
+++ Macros/Expander.C
@@ -0,0 +1,68 @@
+std::vector<double> GetAxisEdges(TH1C *hist){
+  std::vector<double> returnArray;
+  //cout<<"bins: "<<hist->GetNbinsX()<<endl;
+  for(Int_t ib=1; ib<hist->GetNbinsX()+2; ib++){
+    //cout<<"edges: "<<hist->GetBinLowEdge(ib)<<endl;
+    returnArray.emplace_back(hist->GetBinLowEdge(ib));
+  }
+  return returnArray;
+}
+
+THnF* GetTHnF(TFile *file, TString newHistoName = "newHistogram", TString histoName = "correlate-strangeness/sameEvent/Signal/K0Short"){
+  // returns THnF with expanded axes
+  // step 1: grab the necessary 1D axes from the reference TH1Cs
+  TH1C *hDPhi = (TH1C*) file->Get("correlate-strangeness/axes/hDeltaPhiAxis");
+  TH1C *hDEta = (TH1C*) file->Get("correlate-strangeness/axes/hDeltaEtaAxis");
+  TH1C *hPtAssoc = (TH1C*) file->Get("correlate-strangeness/axes/hPtAssocAxis");
+  TH1C *hVtxZ = (TH1C*) file->Get("correlate-strangeness/axes/hVertexZAxis");
+  TH1C *hMult = (TH1C*) file->Get("correlate-strangeness/axes/hMultAxis");
+  
+  std::vector<std::vector<double>> expandedAxes;
+  std::vector<double> axisDPhi = GetAxisEdges(hDPhi);
+  std::vector<double> axisDEta = GetAxisEdges(hDEta);
+  std::vector<double> axisPtAssoc = GetAxisEdges(hPtAssoc);
+  std::vector<double> axisVtxZ = GetAxisEdges(hVtxZ);
+  std::vector<double> axisMult = GetAxisEdges(hMult);
+  
+  expandedAxes.emplace_back(axisDPhi);
+  expandedAxes.emplace_back(axisDEta);
+  expandedAxes.emplace_back(axisPtAssoc);
+  expandedAxes.emplace_back(axisVtxZ);
+  expandedAxes.emplace_back(axisMult);
+  
+  Int_t nbins[5];
+  nbins[0] = hDPhi->GetNbinsX();
+  nbins[1] = hDEta->GetNbinsX();
+  nbins[2] = hPtAssoc->GetNbinsX();
+  nbins[3] = hVtxZ->GetNbinsX();
+  nbins[4] = hMult->GetNbinsX();
+  
+  //  The constructor we want to use
+  //  THn(const char *name, const char *title, Int_t dim, const Int_t *nbins,
+  //      const std::vector<std::vector<double>> &xbins);
+  THnF *hReturnHisto = new THnF(newHistoName.Data(), "", 5, nbins, expandedAxes);
+  
+  // get the information to populate
+  cout<<"Loading and expanding histogram: "<<histoName.Data()<<endl;
+  THnF *hNd = (THnF*) file->Get(histoName.Data());
+  
+  Int_t coordinate[5];
+  Int_t coordinateNew[5];
+  double content;
+  
+  for(Int_t i1=0; i1<nbins[0]; i1++){
+    for(Int_t i2=0; i2<nbins[1]; i2++){
+      for(Int_t i3=0; i3<nbins[2]; i3++){
+        for(Int_t i4=0; i4<nbins[3]; i4++){
+          for(Int_t i5=0; i5<nbins[4]; i5++){
+            coordinate[0] = i1; coordinate[1] = i2; coordinate[2] = i3; coordinate[3] = i4; coordinate[4] = i5;
+            coordinateNew[0] = i1+1; coordinateNew[1] = i2+1; coordinateNew[2] = i3+1; coordinateNew[3] = i4+1; coordinateNew[4] = i5+1;
+            content = hNd->GetBinContent(coordinate);
+            hReturnHisto->SetBinContent(coordinateNew, content);
+          }
+        }
+      }
+    }
+  }
+  return hReturnHisto;
+} 
